732c81bce7731ab8ae0dbe71d78074b0551a4c87
Dursun <dursungokoglan@gmail.com>
2023-03-08 00:16:15 +0300

    Bug that occurs when user types a shift value greater than 26 has been solved

--- lecture 88/lecture 88(self).py
+++ lecture 88/lecture 88(self).py
@@ -8,6 +8,8 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+shift %= 26
+
 def encrypt(t = text, s = shift):
 
     result = ""
