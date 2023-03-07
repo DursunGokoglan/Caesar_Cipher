5e419b199da2ffe0c9a7c1cf31c141e3c06565e5
Dursun <dursungokoglan@gmail.com>
2023-03-07 23:54:31 +0300

    Logo added

--- lecture 88/lecture 88(self).py
+++ lecture 88/lecture 88(self).py
@@ -1,3 +1,7 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -10,8 +14,7 @@ def encrypt(t = text, s = shift):
 
     for l in t:
         if l not in alphabet:
-            new = l
-            result += new
+            result += l
         else:
             position = alphabet.index(l)
             new = alphabet[position + s]
@@ -24,8 +27,7 @@ def decrypt(t = text, s = shift):
 
     for l in t:
         if l not in alphabet:
-            new = l
-            result += new
+            result += l
         else:
             position = alphabet.index(l)
             new = alphabet[position - s]
